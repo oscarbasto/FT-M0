3788c6cdd33e50efd94f42872b81d064e7cf9d10
Oscar Basto <oscarbasto5627@gmail.com>
2021-12-03 00:59:41 -0500

    finish Homework js2

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -4,6 +4,9 @@ function mayuscula(nombre) {
   //La función recibe un nombre y debe devolver el mismo que recibe pero con su primer letra en mayúscula
   //ej: Recibe "mario" ----> Devuelve "Mario"
   //Tu código:
+    return nombre[0].toUpperCase() + nombre.slice(1);
+  
+
 }
 
 function invocarCallback(cb) {
